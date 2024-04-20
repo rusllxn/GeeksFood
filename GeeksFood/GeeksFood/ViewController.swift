c858341caf0fe44d484daf12bac8299aa2b3bc02
rusllxn <rusllxn@gmail.com>
2024-04-20 18:32:25 +0600

    Added .gitignore file

---
+++ GeeksFood/GeeksFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeeksFood
+//
+//  Created by Руслан Канжарбеков on 20.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
